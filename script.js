89bac69e0b415f7e2d110549e1b6c91dc6dd547f
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-04-27 10:31:05 +0300

    cost-section

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ tabHeaders.forEach(function (item) {
     contentBox.classList.remove("hiden");
   });
 });
+
+
+// menu
+
+function navToggle() {
+	document.getElementById("headerUl").classList.toggle("_active");
+	document.getElementById("html").classList.toggle("_overflow");
+}
+
+//time
+window.onload = function() {
+    window.setInterval(function(){
+        var date = new Date();
+
+
+        var years = date.getFullYear();
+        
+
+        var clock = years;
+
+        document.getElementById("year-clock").innerHTML = clock; 
+    }
+    );
+}
